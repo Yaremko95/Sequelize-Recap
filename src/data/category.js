a5cee9646f8d498c13d0292c9d37f0e9628acc89
Tetiana Yaremko <tetianayaremko@Tetianas-MacBook-Pro.local>
2021-11-05 13:07:10 +0100

    adds bilerplate

--- src/data/category.js
+++
@@ -1,8 +0,0 @@
-export const categories = [
-  { name: "react", isProgramming: true },
-  { name: "sql", isProgramming: true },
-  { name: "mongoDB", isProgramming: true },
-  { name: "backend", isProgramming: true },
-  { name: "frontend", isProgramming: true },
-  { name: "movies", isProgramming: false },
-];
--- src/data/reviews.js
+++
@@ -1,5 +0,0 @@
-export const reviews = [
-  { text: "this is cool" },
-  { text: "don't like it" },
-  { text: "very nice" },
-];
